a1157bb9ea1997e9e7d3244d692006124c77ea77
Gift <geezyenyoghasim@gmail.com>
2022-10-03 12:27:39 +0100

    worked on react test 01

--- jsx-tests/src/test-01.tsx
+++ jsx-tests/src/test-01.tsx
@@ -1,25 +1,35 @@
 /**
- * In the following React template, display an unordered list (UL) with list items (LI) within it. 
- * The content of each list item should contain two spans (SPAN), one with the name and the other with the age passed in to the DataList function. 
+ * In the following React template, display an unordered list (UL) with list items (LI) within it.
+ * The content of each list item should contain two spans (SPAN), one with the name and the other with the age passed in to the DataList function.
  * The span elements should be separated by a single space.
  */
 
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 function DataList(props) {
   return (
-    <h2>code goes here</h2>
+    <>
+      <h2>code goes here</h2>
+
+      <ul>
+        {props.data &&
+          props.data.map((data) => (
+            <>
+              <li>
+                <span>{data.name}</span>&nbsp;<span>{data.age}</span>
+              </li>
+            </>
+          ))}
+      </ul>
+    </>
   );
 }
 
 const data = [
-  { name: 'Daniel', age: 25 },
-  { name: 'John', age: 24 },
-  { name: 'Jen', age: 31 },
+  { name: "Daniel", age: 25 },
+  { name: "John", age: 24 },
+  { name: "Jen", age: 31 },
 ];
 
-ReactDOM.render(
-  <DataList data={ data } />,
-  document.getElementById('test-01')
-);
+ReactDOM.render(<DataList data={data} />, document.getElementById("test-01"));
