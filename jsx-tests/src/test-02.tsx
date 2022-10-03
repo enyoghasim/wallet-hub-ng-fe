69117d2a8088ce2d92d63abad2b1c2a124cbc75e
Gift <geezyenyoghasim@gmail.com>
2022-10-03 12:33:28 +0100

    worked on react test 02

--- jsx-tests/src/test-02.tsx
+++ jsx-tests/src/test-02.tsx
@@ -1,27 +1,34 @@
 /**
- * In the following React template, modify the component so that the counter correctly displays and it increments by one whenever the button is pressed. 
+ * In the following React template, modify the component so that the counter correctly displays and it increments by one whenever the button is pressed.
  * You are free to add classes and styles, but make sure you leave the element ID's as they are.
  */
 
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-class Counter extends React.Component {
+class Counter extends React.Component<{}, { count: number }> {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+    };
   }
 
   render() {
     return (
       <div id="mainArea">
-        <p>button count: <span>0</span></p>
-        <button id="mainButton">Increase</button>
+        <p>
+          button count: <span>{this.state.count}</span>
+        </p>
+        <button
+          id="mainButton"
+          onClick={() => this.setState({ count: this.state.count + 1 })}
+        >
+          Increase
+        </button>
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <Counter />,
-  document.getElementById('test-02')
-);
+ReactDOM.render(<Counter />, document.getElementById("test-02"));
